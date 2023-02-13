36e5ee25aab010cc3723344f11f617dd6a47233e
homumado <nobisan@gmail.com>
2023-02-12 18:35:06 -1000

    match core

--- app/i18n/i18next.client.ts
+++ app/i18n/i18next.client.ts
@@ -15,7 +15,7 @@ export async function initI18nextClient(hydrate: IdleRequestCallback) {
         ...config, // spread the configuration
         // This function detects the namespaces your routes rendered while SSR use
         ns: getInitialNamespaces(),
-        backend: { loadPath: "/locales/{{lng}}/{{ns}}.json" },
+        backend: { loadPath: "/src/locales/{{lng}}/{{ns}}.json" },
         detection: {
           // Here only enable htmlTag detection, we'll detect the language only
           // server-side with remix-i18next, by using the `<html lang>` attribute
